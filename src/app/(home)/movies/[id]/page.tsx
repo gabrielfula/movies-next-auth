4c32738b8cb6918b7fdd9fa455dba58275c3ec28
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-10-30 09:15:11 -0300

    feat: add suspense fallback

--- src/app/(home)/movies/[id]/page.tsx
+++ src/app/(home)/movies/[id]/page.tsx
@@ -7,6 +7,7 @@ import {
 } from "@/components/ui/card";
 import { GetIndividual } from "@/service/api";
 import Image from "next/image";
+import { Suspense } from "react";
 
 interface MoviesIndividualProps {
   params: {
@@ -20,28 +21,32 @@ export default async function MoviesIndividual({
   const movie = await GetIndividual(params.id);
 
   return (
-    <Card className="flex flex-col items-center lg:flex-row lg:items-start">
-      <CardHeader>
-        <Image
-          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          width={950}
-          height={400}
-          alt="Movie"
-        />
-      </CardHeader>
-      <div className="flex flex-col lg:items-start justify-center items-center gap-5 lg:justify-start lg:pt-4">
-        <div className="flex gap-4 items-center ">
-          <CardTitle className="text-2xl">{movie.title}</CardTitle>
-          <CardDescription className="text-sm">
-            {movie.release_date}
-          </CardDescription>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Card className="flex flex-col items-center lg:flex-row lg:items-start">
+        <CardHeader>
+          <Suspense fallback={<p>Loading...</p>}>
+            <Image
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              width={950}
+              height={400}
+              alt="Movie"
+            />
+          </Suspense>
+        </CardHeader>
+        <div className="flex flex-col lg:items-start justify-center items-center gap-5 lg:justify-start lg:pt-4">
+          <div className="flex gap-4 items-center ">
+            <CardTitle className="text-2xl">{movie.title}</CardTitle>
+            <CardDescription className="text-sm">
+              {movie.release_date}
+            </CardDescription>
+          </div>
+          <div className="pt-4">
+            <CardDescription className="text-base lg:w-10/12 w-full text-zinc-400">
+              {movie.overview}
+            </CardDescription>
+          </div>
         </div>
-        <div className="pt-4">
-          <CardDescription className="text-base lg:w-10/12 w-full text-zinc-400">
-            {movie.overview}
-          </CardDescription>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </Suspense>
   );
 }
