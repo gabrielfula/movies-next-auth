2d2264cf643e95900e43777320a13ca2651af391
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-10-30 08:30:24 -0300

    feat: create individual movie page

--- src/app/(home)/movies/[id]/page.tsx
+++ src/app/(home)/movies/[id]/page.tsx
@@ -1,4 +1,12 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { GetIndividual } from "@/service/api";
+import Image from "next/image";
 
 interface MoviesIndividualProps {
   params: {
@@ -11,12 +19,27 @@ export default async function MoviesIndividual({
 }: MoviesIndividualProps) {
   const movie = await GetIndividual(params.id);
 
-  console.log(movie);
-
   return (
-    <div>
-      <p>MoviesIndividual</p>
-      <p>{params.id}</p>
-    </div>
+    <Card className="flex flex-col items-center">
+      <CardHeader>
+        <Image
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          width={450}
+          height={400}
+          alt="Movie"
+        />
+        <div className="flex gap-4 items-center">
+          <CardTitle className="text-2xl">{movie.title}</CardTitle>
+          <CardDescription className="text-sm">
+            {movie.release_date}
+          </CardDescription>
+        </div>
+        <div className="pt-4">
+          <CardDescription className="text-base">
+            {movie.overview}
+          </CardDescription>
+        </div>
+      </CardHeader>
+    </Card>
   );
 }
