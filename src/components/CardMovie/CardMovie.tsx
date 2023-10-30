6fd689501511fcdfe359b7df32c5173d3d0e9b75
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-10-30 09:19:53 -0300

    style: add cursor-pointer

--- src/components/CardMovie/CardMovie.tsx
+++ src/components/CardMovie/CardMovie.tsx
@@ -4,13 +4,9 @@ import { MoviesType } from "@/types/types";
 import Image from "next/image";
 import { Card } from "../ui/card";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
-export default function CardMovie({
-  title,
-  overview,
-  poster_path,
-  id,
-}: MoviesType) {
+export default function CardMovie({ title, poster_path, id }: MoviesType) {
   const { push } = useRouter();
 
   const handlePage = (id: number) => {
@@ -19,14 +15,16 @@ export default function CardMovie({
 
   return (
     <Card className="w-full flex flex-col items-center text-center">
-      <Image
-        src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
-        className="rounded"
-        height={200}
-        width={200}
-        alt="Posters Movies"
-        onClick={() => handlePage(id)}
-      />
+      <Link href="/">
+        <Image
+          src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+          className="rounded cursor-pointer"
+          height={200}
+          width={200}
+          alt="Posters Movies"
+          onClick={() => handlePage(id)}
+        />
+      </Link>
 
       <p className="truncate w-[9rem] text-sm">{title}</p>
       {/* <p>OverView...</p> */}
