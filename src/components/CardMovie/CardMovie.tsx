452fdd43bdb64b54394a8e0b5c522e731f42c058
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-11-02 07:47:22 -0300

    fix: remove "link" in Image

--- src/components/CardMovie/CardMovie.tsx
+++ src/components/CardMovie/CardMovie.tsx
@@ -15,16 +15,14 @@ export default function CardMovie({ title, poster_path, id }: MoviesType) {
 
   return (
     <Card className="w-full flex flex-col items-center text-center">
-      <Link href="/">
-        <Image
-          src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
-          className="rounded cursor-pointer"
-          height={200}
-          width={200}
-          alt="Posters Movies"
-          onClick={() => handlePage(id)}
-        />
-      </Link>
+      <Image
+        src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+        className="rounded cursor-pointer"
+        height={200}
+        width={200}
+        alt="Posters Movies"
+        onClick={() => handlePage(id)}
+      />
 
       <p className="truncate w-[9rem] text-sm">{title}</p>
       {/* <p>OverView...</p> */}
