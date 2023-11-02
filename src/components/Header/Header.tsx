1b91c870314762339d50f73e155ff33908bd8eea
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-11-02 07:57:19 -0300

    style: add hover and padding on items

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,40 +10,54 @@ import {
 } from "../ui/sheet";
 import Link from "next/link";
 import Login from "../Login/Login";
+import { Input } from "../ui/input";
 
 // useSession
 
 export default function Header() {
   return (
-    <Card className="w-full flex justify-between items-center">
+    <Card className="w-full flex justify-between items-center pr-10">
       <Sheet>
         <SheetTrigger>
           <AlignLeft />
         </SheetTrigger>
         <SheetContent className="w-[400px] sm:w-[540px]" side={"left"}>
-          <SheetHeader className="space-y-4">
+          <SheetHeader className="space-y-6">
             <SheetTitle className="text-zinc-200">Menu</SheetTitle>
-            <SheetDescription>
-              <a href="">Aventura</a>
+            <SheetDescription className="hover:text-zinc-50 transition-all">
+              <Login />
             </SheetDescription>
             <SheetDescription>
-              <a href="">Ação</a>
+              <a href="" className="hover:text-zinc-50 transition-all">
+                Aventura
+              </a>
             </SheetDescription>
             <SheetDescription>
-              <a href="">Infantil</a>
+              <a href="" className="hover:text-zinc-50 transition-all">
+                Ação
+              </a>
             </SheetDescription>
             <SheetDescription>
-              <a href="">Filmes</a>
+              <a href="" className="hover:text-zinc-50 transition-all">
+                Infantil
+              </a>
+            </SheetDescription>
+            <SheetDescription>
+              <a href="" className="hover:text-zinc-50 transition-all">
+                Filmes
+              </a>
             </SheetDescription>
           </SheetHeader>
         </SheetContent>
       </Sheet>
       <div>
-        <Link href="/" className="text-lg">
+        <Link href="/" className="text-xl">
           <span className="text-red-700">Fula</span>Movies
         </Link>
       </div>
-      <Login />
+      <div className="flex gap-10 items-center">
+        <Input type="email" placeholder="Email" className="h-8" />
+      </div>
     </Card>
   );
 }
