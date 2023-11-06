b3df0352a03d4e8bd51888c364a571c2060eff4b
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-11-06 09:16:18 -0300

    feat: create google login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,23 +1,39 @@
 "use client";
 
-import { LogIn } from "lucide-react";
+import { LogIn, LogOut } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import { Avatar, AvatarImage } from "../ui/avatar";
 
 export default function Login() {
-  const { data: session } = useSession();
+  const { status, data } = useSession();
 
   return (
     <>
-      {!session ? (
-        <div
-          className="flex text-sm gap-2 items-center cursor-pointer"
-          onClick={() => signIn()}
-        >
-          <p>Fazer Login</p>
-          <LogIn />
+      {status === "authenticated" && (
+        <div>
+          <div className="flex gap-3">
+            <Avatar>
+              <AvatarImage src={data.user?.image} />
+            </Avatar>
+            <p>{data.user?.name}</p>
+          </div>
+          <div className="mt-5" onClick={() => signOut()}>
+            <button className="flex gap-3 text-sm">
+              Fazer Logout <LogOut size={20} />
+            </button>
+          </div>
         </div>
-      ) : (
-        <h1>Usuario</h1>
+      )}
+      {status === "unauthenticated" && (
+        <p>
+          <div
+            className="flex text-sm gap-2 items-center cursor-pointer"
+            onClick={() => signIn("google")}
+          >
+            <p>Fazer Login</p>
+          </div>
+        </p>
       )}
     </>
   );
