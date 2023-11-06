74985d7a1bb0d3469e040e156886d5d326d8cc69
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-11-06 19:39:21 -0300

    feat: create component to search

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import { Search } from "lucide-react";
+
+import { useRouter } from "next/navigation";
+
+export default function SearchMovie() {
+  const [value, setValue] = useState("");
+
+  const { push } = useRouter();
+
+  const handleClick = () => {
+    push(`/search/${value}`);
+    setValue("");
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search Movie..."
+        className="outline-none border-none rounded-none bg-none focus:outline-none pl-2 text-black w-full"
+        onChange={(event) => setValue(event.target.value)}
+        value={value}
+      />
+      <button
+        className="outline-none bg-none border-none"
+        onClick={handleClick}
+      >
+        <Search color="black" />
+      </button>
+    </>
+  );
+}
